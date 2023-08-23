4f570fbf72432be7a819709558744ec1fae1a5b0
3top1a <3top1a.official@gmail.com>
2023-08-23 15:05:08 +0200

    216b, relocation-model=static (no PIE again)

--- check.sh
+++ check.sh
@@ -5,15 +5,15 @@
 # rustup component add rust-src --toolchain nightly
 
 
-# -Clink-arg=-nostartfiles
-RUSTFLAGS="-Ctarget-cpu=native -Clink-arg=-nostartfiles -Ctarget-feature=+crt-static -Clink-args=-Wl,-n,-N,--no-dynamic-linker,--no-pie,-build-id=none" cargo +nightly b --release 
+RUSTFLAGS="-Ctarget-cpu=native -Clink-arg=-nostartfiles -Ctarget-feature=+crt-static -C relocation-model=static -Clink-args=-Wl,-n,-N,--no-dynamic-linker,--no-pie,-build-id=none" cargo +nightly b --release 
 
 # Processing
 # Remove unnecesary sectors
 objcopy -R .shstrtab -R .comment ./target/release/smallest-hello ./target/release/smallest-hello.tmp
 mv ./target/release/smallest-hello.tmp ./target/release/smallest-hello
 
-# Remove everything after Hello Rust!\n\x00
+# Remove everything after Hello Rust!\n\x00, mainly the section header
+# Ideally you would use something like https://github.com/blackle/Section-Header-Stripper
 python truncate.py
 
 echo
